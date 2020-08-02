df541b6ef9520e70a686488bbc450f8a34d7000e
Rishabh Singh <rishabh.1056@gmail.com>
2020-08-02 20:47:56 +0530

    > Minor changes

--- app/src/main/java/com/example/fitness2020channelapp/Activities/InfoActivity.java
+++ app/src/main/java/com/example/fitness2020channelapp/Activities/InfoActivity.java
@@ -48,6 +48,26 @@ public class InfoActivity extends AppCompatActivity {
         initAdapter();
         setAdapters();
 
+        centerDescripton.setText("Capoeira India Centre For Capoeira India is " +
+                "India's first academy for teaching capoeira, the Brazilian martial art. " +
+                "Started by Professor Reza \"Baba\" Massah with one centre in Mumbai in 2005, " +
+                "the Centre For Capoeira India now runs centres – teaching capoeira to kids and" +
+                " adults – across 9 locations in Mumbai, Delhi and Jaipur. Apart from private centres," +
+                " Centre For Capoeira India also runs in-school capoeira programs, as part of the school " +
+                "curriculum, in 10 international schools across Mumbai. Centre For Capoeira India also " +
+                "produces capoeira performances, combining the various elements of capoeira – movement, " +
+                "acrobatics, dance, samba, music – at select events, using these as an opportunity to" +
+                " showcase the skills of our students and also demonstrate the beauty, grace, rhythm and " +
+                "discipline of capoeira. For any enquiries about any of our programs, please contact Aparna " +
+                "Massah on aparna@capoeiraindia.in or +918652726738. Check us out: " +
+                "https://www.facebook.com/CentreForCapoeiraIndia/ https://www.capoeiraindia.in/ ");
+
+        centrTnC.setText("Amet occaecat nisi sint in deserunt minim ex " +
+                "pariatur consectetur consectetur ullamco ipsum. Non Lorem" +
+                " dolore qui enim anim aliquip ad minim culpa excepteur " +
+                "ad commodo sunt. Dolor in qui nisi amet aliquip proident. " +
+                "Do cupidatat et sit do proident.");
+
         backBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -133,15 +153,15 @@ public class InfoActivity extends AppCompatActivity {
     }
 
     private void attachID() {
-//        addressRV = findViewById(R.id.);
-//        backBtn = findViewById(R.id.);
-//        saveInfoBtn = findViewById();
-//        centerName = findViewById();
-//        centerDescripton = findViewById();
-//        centrTnC = findViewById();
-//        addAddressBtn = findViewById();
-//        editNameBtn = findViewById();
-//        editDescriptionBtn = findViewById();
-//        editTnCBtn = findViewById();
+        addressRV = findViewById(R.id.info_address_rv);
+        backBtn = findViewById(R.id.back_btn);
+        saveInfoBtn = findViewById(R.id.info_save_tv);
+        centerName = findViewById(R.id.info_name);
+        centerDescripton = findViewById(R.id.info_description_text_tv);
+        centrTnC = findViewById(R.id.info_remember_text_tv);
+        addAddressBtn = findViewById(R.id.info_add_address_btn);
+        editNameBtn = findViewById(R.id.info_name_edit_imgvw);
+        editDescriptionBtn = findViewById(R.id.info_description_edit_btn);
+        editTnCBtn = findViewById(R.id.info_remember_edit_btn);
     }
 }
