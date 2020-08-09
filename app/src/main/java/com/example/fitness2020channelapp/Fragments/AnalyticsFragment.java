363ee17e18f487e0db6ac44fc9079e7e6d8eae9a
ayush-sharma2601 <ayushsh2601@gmail.com>
2020-08-09 13:06:26 +0530

    AnalyticsFragment java implementation done

--- app/src/main/java/com/example/fitness2020channelapp/Fragments/AnalyticsFragment.java
+++ app/src/main/java/com/example/fitness2020channelapp/Fragments/AnalyticsFragment.java
@@ -1,19 +1,31 @@
 package com.example.fitness2020channelapp.Fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.RelativeLayout;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.fitness2020channelapp.Activities.AnalyticsDetailsActivity;
 import com.example.fitness2020channelapp.R;
 
 public class AnalyticsFragment extends Fragment {
 
     View view;
+    TextView lastMonthRevenue,currentRevenue,lastMonthName,currentVisits,pageViewCount,directionReqCount,checkinCount,bookmarkCount;
+    RelativeLayout analyticsDetailBtn,newCampaignBtn;
+    Spinner monthSlotSpinner;
+    String[] months = {"Tue 19 Jan ‘20 - Thu 18 Feb ‘20","Tue 19 Feb ‘20 - Thu 18 Dec ‘20",
+            "Tue 19 Jan ‘20 - Thu 18 Feb ‘20","Tue 19 Jan ‘20 - Thu 18 Feb ‘20","Tue 19 Jan ‘20 - Thu 18 Feb ‘20","Tue 19 Jan ‘20 - Thu 18 Feb ‘20"};
 
     public AnalyticsFragment() {
     }
@@ -25,10 +37,57 @@ public class AnalyticsFragment extends Fragment {
         view=inflater.inflate(R.layout.fragment_analytics,container,false);
 
         attachID();
+        initSpinner();
+
+        analyticsDetailBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(view.getContext(), AnalyticsDetailsActivity.class);
+                view.getContext().startActivity(intent);
+            }
+        });
+
+        newCampaignBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //do start campaign thing
+            }
+        });
 
         return view;
     }
 
+    private void initSpinner() {
+        ArrayAdapter monthAdapter = new ArrayAdapter(view.getContext(),android.R.layout.simple_spinner_dropdown_item,months);
+        monthAdapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        monthSlotSpinner.setAdapter(monthAdapter);
+        monthSlotSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+
     private void attachID() {
+//        lastMonthName = view.findViewById(R.id.);
+//        lastMonthRevenue = view.findViewById(R.id.);
+//        currentRevenue = view.findViewById(R.id.);
+//        currentVisits = view.findViewById(R.id.);
+//        pageViewCount = view.findViewById(R.id.);
+//        directionReqCount = view.findViewById(R.id.);
+//        checkinCount = view.findViewById(R.id.);
+//        bookmarkCount = view.findViewById(R.id.);
+//        analyticsDetailBtn = view.findViewById(R.id.);
+//        newCampaignBtn = view.findViewById(R.id.);
+//        monthSlotSpinner = view.findViewById(R.id.);
+
+
+
     }
 }
